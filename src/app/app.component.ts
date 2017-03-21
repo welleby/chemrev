1d95df97db7791e056114050ceb2d3d22a60d710
welleby <tommywelleby@gmail.com>
2017-03-21 16:35:59 +0100

    Moved test data from AppComponent to UsersComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,32 +8,8 @@ import 'hammerjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
 
-  screenshotService;
-  maxStuff:number;
   title = 'Chemical Measurements';
-  spaceScreens: Array<any>;
-
-    constructor(private http:Http) {
-      this.screenshotService = new ScreenshotService(http);
-    };
-
-    ngOnInit(): void {
-      this.screenshotService.getScreenshots()
-        .subscribe(resp => this.spaceScreens = resp.screenshots);
-    };
-
-    likeMe(i){
-      this.spaceScreens[i].liked = !this.spaceScreens[i].liked;
-      console.log(this.spaceScreens[i].liked);
-      /*if(this.spaceScreens[i].liked == 0)  
-        this.spaceScreens[i].liked = 1;
-      else 
-        this.spaceScreens[i].liked = 0;*/
-    }
-    deleteMe(i){
-      this.spaceScreens.splice(i,1);
-      console.log(i);
-    }
+  
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ScreenshotService } from "app/screenshot.service";
+import { Http } from "@angular/http";
 
 @Component({
   selector: 'app-users',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  screenshotService;
+  maxStuff:number;
+  spaceScreens: Array<any>;
 
-  ngOnInit() {
-  }
+    constructor(private http:Http) {
+      this.screenshotService = new ScreenshotService(http);
+    };
+
+    ngOnInit(): void {
+      this.screenshotService.getScreenshots()
+        .subscribe(resp => this.spaceScreens = resp.screenshots);
+    };
+
+    likeMe(i){
+      this.spaceScreens[i].liked = !this.spaceScreens[i].liked;
+      console.log(this.spaceScreens[i].liked);
+      /*if(this.spaceScreens[i].liked == 0)  
+        this.spaceScreens[i].liked = 1;
+      else 
+        this.spaceScreens[i].liked = 0;*/
+    }
+    deleteMe(i){
+      this.spaceScreens.splice(i,1);
+      console.log(i);
+    }
 
 }
